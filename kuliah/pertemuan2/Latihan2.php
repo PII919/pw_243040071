a02ca8c3735e4a7fb5f7bcefa56fb2ac8a4e0978
PII919 <faisal.mulki15@gmail.com>
2025-02-17 19:41:53 +0700

    menyelesaikan tugas 1

---
+++ kuliah/pertemuan2/Latihan2.php
@@ -0,0 +1,12 @@
+<?php
+$nama = "Mochamad Raffi Khoirunnijam";
+$email = "faisal.mulki15@gmail.com";
+$pekerjaan = "mahasiswa";
+$jurusan = "Teknik Informatika";
+$universitas = "UNPAS";
+
+ echo "<h1>$nama</h1>";
+ echo "<P>$email</p>";
+
+ echo "Halo, nama saya $nama, saya $pekerjaan di $jurusan $universitas";
+ ?>
---
+++ tugas/tugas1/1c.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1C</title>
+    <style>
+        .container {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            gap: px;
+            margin: 20px;
+        }
+
+        .row {
+            display: flex;
+            
+        }
+
+        .box {
+            width: 150px;
+            height: 150px;
+            background-color:rgb(115, 96, 200);
+            color:rgb(171, 154, 222);
+            font-size: 20px;
+            font-weight: bold;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border: 2px solid black;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="box">1</div>
+        </div>
+        <div class="row">
+            <div class="box">2</div>
+            <div class="box">2</div>
+        </div>
+        <div class="row">
+            <div class="box">3</div>
+            <div class="box">3</div>
+            <div class="box">3</div>
+        </div>
+    </div>
+
+</body>
+</html>
