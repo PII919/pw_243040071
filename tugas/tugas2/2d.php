9664dc15705268cb0263774e773803e6b03e6be5
PII919 <faisal.mulki15@gmail.com>
2025-02-24 17:07:19 +0700

    menyelesaikan tugas 2

---
+++ tugas/tugas2/2d.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2d.php</title>
+    <style>
+        .container {
+            display: grid;
+            grid-template-columns: repeat(4, 50px);
+            grid-template-rows: repeat(4, 50px);
+            border: 4px solid black; 
+            width: fit-content;
+        }
+        .kotak {
+            width: 50px;
+            height: 50px;
+            border: 1px solid black; 
+        }
+        .hitam {
+            background-color: black;
+        }
+        .putih {
+            background-color: white;
+        }
+    </style>
+</head>
+<body>
+
+<div class="container">
+<?php
+for ($i = 0; $i < 4; $i++) {
+    for ($j = 0; $j < 4; $j++) {
+        $warna = ($i + $j) % 2 == 0 ? "putih" : "hitam";
+        echo "<div class='kotak $warna'></div>";
+    }
+}
+?>
+</div>
+
+</body>
+</html>
